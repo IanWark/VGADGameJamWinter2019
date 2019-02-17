cb91d5c409a52527b835ec23eb1f10090cee6d7f
IanWark <wark@ualberta.ca>
2019-02-17 11:42:02 -0700

    create events folder in art, scripts, and scenes

--- scripts/events/event_1_a.gd
+++ scripts/events/event_1_a.gd
@@ -1,10 +1,9 @@
-extends "res://scripts/sprite.gd"
+extends "res://scripts/event.gd"
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	scene = "res://scenes/event_unknown.tscn"
-	locked_event = true
+	scene = "res://scenes/events/event_1_a.tscn"
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
