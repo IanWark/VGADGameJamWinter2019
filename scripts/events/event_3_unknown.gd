f7a51f0e9659bfb7e6d1412ad74124f7abf65b63
IanWark <wark@ualberta.ca>
2019-02-18 09:37:53 -0700

    fix wirecutters typo

--- scripts/events/event_3_unknown.gd
+++ scripts/events/event_3_unknown.gd
@@ -5,5 +5,5 @@ func _ready():
 	scene = "res://scenes/events/event_3_unknown.tscn"
 	correct_event = "event_3_a"
 	banned_strings = ["crowbar", "clock"]
-	required_strings = ["panel", "wire_cutters"]
+	required_strings = ["panel", "wirecutters"]
 	set_locked()
