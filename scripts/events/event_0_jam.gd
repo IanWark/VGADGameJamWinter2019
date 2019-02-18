79a9ca5186aeaab66d05bcdf9ab4b9e7a6402c8b
IanWark <wark@ualberta.ca>
2019-02-17 20:31:25 -0700

    Change animations to move on/off the board

--- scripts/events/event_0_jam.gd
+++ scripts/events/event_0_jam.gd
@@ -5,8 +5,7 @@ func _ready():
 	scene = "res://scenes/events/event_0_jam.tscn"
 	set_locked()
 	locked_event = true
+	
+	anim_player.stop()
+	position = Vector2(0, 0)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
