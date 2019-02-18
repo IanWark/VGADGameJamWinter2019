243079d2d999b0d5048d7a87bb955dd803d72d8d
IanWark <wark@ualberta.ca>
2019-02-18 02:10:49 -0700

    fix event_2_unknown scene

--- scripts/events/event_2_unknown.gd
+++ scripts/events/event_2_unknown.gd
@@ -2,6 +2,6 @@ extends "res://scripts/events/event_unknown.gd"
 
 func _ready():
 	._ready()
-	scene = "res://scenes/events/event_1_unknown.tscn"
+	scene = "res://scenes/events/event_2_unknown.tscn"
 	correct_event = "event_2_a"
 	set_locked()
