2dbc0091aac4511b67ea8dad0ba51f00e8917817
IanWark <wark@ualberta.ca>
2019-02-18 03:46:05 -0700

    Change some event text

--- scripts/events/event_2_a.gd
+++ scripts/events/event_2_a.gd
@@ -1,7 +1,19 @@
 extends "res://scripts/event.gd"
 
 const NORMAL = "He snuck past the guard. But how?"
+const CLOCK = "A jumping minute hand? This guard falls asleep on the job."
 
 func _ready():
 	._ready()
 	scene = "res://scenes/events/event_2_a.tscn"
+
+func set_text():
+	var has_crowbar = clues.has("crowbar")
+	var has_clock = clues.has("clock")
+	
+	if has_crowbar && has_clock:
+		label.text = NORMAL
+	elif has_clock:
+		label.text = CLOCK 
+	else: 
+		label.text = NORMAL
--- scripts/events/event_3_a.gd
+++ scripts/events/event_3_a.gd
@@ -1,7 +1,16 @@
 extends "res://scripts/event.gd"
 
-const NORMAL = "He deactivated the alarm system."
+const NORMAL = "He deactivated the alarm system. But where was it?"
+const PANEL = "He deactivated the alarm system inside the pedestal."
 
 func _ready():
 	._ready()
 	scene = "res://scenes/events/event_2_a.tscn"
+
+func set_text():
+	var has_panel = clues.has("panel")
+	
+	if has_panel:
+		label.text = PANEL 
+	else: 
+		label.text = NORMAL
